de4b56f5e851e540bd919bc3bdbfeb1f4e8acf44
duong pham <phduongc1tantrao2018@gmail.com>
2024-04-16 12:20:33 +0700

    .

--- AppView/Services/FileServices.cs
+++ AppView/Services/FileServices.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Net.Http.Headers;
 
 namespace AppView.Services
 {
     public class FileServices : IFileServices
     {
-        public FileServices()
+        private readonly HttpClient _http;
+
+        public FileServices(HttpClient http)
         {
-            
+            _http = http;
         }
-        public Task UploadFiles(IBrowserFile file)
+        public async Task UploadFiles(IBrowserFile file)
         {
-            throw new NotImplementedException();
+            var content = new MultipartFormDataContent();
+            var streamContent = new StreamContent(file.OpenReadStream(file.Size));
+
+            streamContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+
+            content.Add(streamContent, "file", file.Name);
+
+            var response = await _http.PostAsync("https://localhost:7167/api/UploadFile", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(await response.Content.ReadAsStringAsync());
+            }
         }
     }
 }
